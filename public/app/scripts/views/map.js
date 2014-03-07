693efca79a6f11fb862e3039e33f1617d1edcde1
David Gonzalez <david@simbiotica.es>
2014-03-07 13:06:28 +0100

    map colors

--- public/app/scripts/views/map.js
+++ public/app/scripts/views/map.js
@@ -58,17 +58,27 @@ define([
     },
 
     changeVisualization: function() {
-      var self, indicator, sql, cartocss, options, colors;
-      colors=['#088246','#379d4e','#66b757','#95d25f','#b1de79','#cce994','#e8f5ae','#fff8c3','#fddc9f','#fbbe79','#faa052','#f8822c','#ef632b','#e7452b','#de262a']
+      var self, indicator, sql, cartocss, options;
 
 
       self = this;
 
       indicator = this.indicator.toJSON();
 
-      sql = sprintf('WITH indicator AS (SELECT * FROM get_agg_geo(\'%1$s\',\'%2$s\',\'%3$s\',\'%4$s\',\'%5$s\')) SELECT g.cartodb_id, g.the_geom, g.geo_id, g.name, g.the_geom_webmercator, i.value, i.percent_change FROM %2$s g LEFT OUTER JOIN indicator i ON (g.geo_id = i.geo_id)', indicator.id, indicator.geog_type1, indicator.date, window.sessionStorage.getItem('token'), new Date());
 
-      cartocss = sprintf('#%s {polygon-fill: #FF0000; line-color: #000; polygon-opacity: 0.8; [value = null] {polygon-fill: #777;}}', '1');
+      var steps=new Array();
+      for (var i=1; i<16; i++){
+        steps.push(indicator.full-(i*indicator.full/8));
+      }
+      var colors=['#088246','#379d4e','#66b757','#95d25f','#b1de79','#cce994','#e8f5ae','#fff8c3','#fddc9f','#fbbe79','#faa052','#f8822c','#ef632b','#e7452b','#de262a'];
+
+      sql = sprintf('WITH indicator AS (SELECT * FROM get_agg_geo(\'%1$s\',\'%2$s\',\'%3$s\',\'%4$s\',\'%5$s\')) SELECT g.cartodb_id, g.the_geom, g.geo_id, g.name, g.the_geom_webmercator, i.value, i.percent_change FROM %2$s g LEFT OUTER JOIN indicator i ON (g.geo_id = i.geo_id)', indicator.id, indicator.geoType1, indicator.date, window.sessionStorage.getItem('token'), new Date());
+
+      cartocss = sprintf('#%s {polygon-fill: #FF0000; line-color: #000; polygon-opacity: 0.8; [percent_change = null] {polygon-fill: #777;}}', '1');
+
+      for (var i = 0, len = steps.length; i < len; i++) {
+        cartocss = cartocss + sprintf(' #%s [percent_change <= %s] {polygon-fill: %s;}', '1', steps[i], colors[i]);
+      }
 
       console.log(indicator, sql, cartocss);
 
