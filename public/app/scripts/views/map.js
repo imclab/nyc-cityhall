f28b0ffd62ff3c00e68f22ca6fb11ea8faed8bc1
David Inga <david.inga@simbiotica.es>
2014-03-07 20:00:59 +0100

    map https

--- public/app/scripts/views/map.js
+++ public/app/scripts/views/map.js
@@ -27,7 +27,6 @@ define([
       },
       cartodb: {
         user_name: 'nyc-cityhall',
-        https: true,
         type: 'cartodb'
       },
       colors: ['#088246', '#379d4e', '#66b757', '#95d25f', '#b1de79', '#cce994', '#e8f5ae', '#fff8c3', '#fddc9f', '#fbbe79', '#faa052', '#f8822c', '#ef632b', '#e7452b', '#de262a']
@@ -99,7 +98,7 @@ define([
         self.map.addLayer(layer);
       }
 
-      cartodb.createLayer(this.map, options).done(onDone);
+      cartodb.createLayer(this.map, options, {https: true}).done(onDone);
     },
 
     open: function(indicator) {
