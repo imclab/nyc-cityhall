68e79cb99336ec02eb18b1200e9314e495a90fd4
David Inga <david.inga@simbiotica.es>
2014-03-14 10:32:37 +0100

    [#30]

--- public/app/scripts/collections/indicators.js
+++ public/app/scripts/collections/indicators.js
@@ -163,7 +163,7 @@ define([
           indicator.value = 0;
           indicator.full = 0;
           indicator.displayValue = '-';
-          indicator.color = '#fff';
+          indicator.color = '#b7c9e4';
         }
 
         indicator.status = (indicator.value / indicator.full >= 0 || indicator.full === 0 || indicator.value === null || indicator.value === 0) ? 'improving' : 'worsening';
