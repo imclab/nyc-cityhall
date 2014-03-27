63afd8b34bc6b918fab87bbfc14329b45af660fc
David Inga <david.inga@simbiotica.es>
2014-03-27 11:57:01 +0100

    infowindow null and zero

--- public/app/scripts/main.js
+++ public/app/scripts/main.js
@@ -41,6 +41,11 @@ require.config({
 
 require(['underscore', 'jquerymobile', 'handlebars', 'moment', 'router'], function(_, $, Handlebars, moment, Router) {
 
+  // Number extension
+  Number.prototype.toCommas = function() {
+    return this.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');;
+  };
+
   // CARTODB Hacks
   cdb.core.Template.compilers = _.extend(cdb.core.Template.compilers, {
     handlebars: typeof(Handlebars) === 'undefined' ? null : Handlebars.compile
@@ -53,22 +58,20 @@ require(['underscore', 'jquerymobile', 'handlebars', 'moment', 'router'], functi
   });
 
   Handlebars.registerHelper('commas', function(context, units) {
-    if (context === null) {
-      return null;
-    }
-
-    context = context.toString();
-    context = context.replace(',', '');
-    context = context.replace('%', '');
-
-    if (context === '-') {
+    if (context === null || context === undefined) {
       return '-';
     }
 
-    context = Number(context);
+    if (typeof context === 'string') {
+      return context;
+    }
 
-    if (context % 1 !== 0) {
-      context = context.toFixed(2);
+    if (context - Math.floor(context) !== 0) {
+      context = Number(context.toFixed(1));
+    }
+
+    if (!units) {
+      return context.toCommas();
     }
 
     if (units === 'percentage') {
@@ -81,7 +84,20 @@ require(['underscore', 'jquerymobile', 'handlebars', 'moment', 'router'], functi
       }
       return '-' + moment().hours(0).minutes(0).seconds(Math.abs(context)).format('HH:mm:ss');
     }
-    return context.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    if (units === 'seconds') {
+      if (context > 0) {
+        return moment().hours(0).minutes(0).seconds(context).format('HH:mm:ss');
+      }
+      return '-' + moment().hours(0).minutes(0).seconds(Math.abs(context)).format('HH:mm:ss');
+    }
+
+    return context.toCommas();
+  });
+
+  Handlebars.registerHelper('xif', function (context, options) {
+    var result = !(context === undefined || context === null);
+    return (result) ? options.fn(this) : options.inverse(this);
   });
 
   new Router();
