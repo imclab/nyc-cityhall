311d02d8cc1ef7dd08926551ec3f610f335a45de
David Inga <david.inga@simbiotica.es>
2014-03-28 13:36:25 +0100

    jshint

--- public/app/scripts/views/map.js
+++ public/app/scripts/views/map.js
@@ -274,10 +274,10 @@ define([
           //cartocss = cartocss + sprintf('#%s {polygon-fill: %s;}', indicator.id, this.options.colors[7]);
 
           if (indicator.full < 0 ) {
-              cartocss = cartocss + sprintf('#%s [%s <= %s] {polygon-fill: %s;}', indicator.id, self.currentData, indicator.full, self.options.colors[0]);
-            } else if (indicator.full > 0 && isFinite(indicator[self.currentData])) {
-              cartocss = cartocss + sprintf('#%s [%s >= %s] {polygon-fill: %s;}', indicator.id, self.currentData, indicator.full, self.options.colors[14]);
-            }
+            cartocss = cartocss + sprintf('#%s [%s <= %s] {polygon-fill: %s;}', indicator.id, self.currentData, indicator.full, self.options.colors[0]);
+          } else if (indicator.full > 0 && isFinite(indicator[self.currentData])) {
+            cartocss = cartocss + sprintf('#%s [%s >= %s] {polygon-fill: %s;}', indicator.id, self.currentData, indicator.full, self.options.colors[14]);
+          }
 
           _.each(this.options.colors, function(color, index) {
             var step = indicator.full - ((index + 1) * indicator.full / 8);
